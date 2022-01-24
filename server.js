c6f2173a79898bbeb419f392529bd9f38885888c
umar saleem <umarraja230@gmail.com>
2022-01-24 19:16:47 +0500

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import db from "./config/db.js";
 import FileRouter from "./routes/files.js";
@@ -9,6 +10,12 @@ import DownloadRouter from "./routes/download.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 db();
+
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWEDCLIENTS.split(","),
+};
+app.use(cors(corsOptions));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = Path.dirname(__filename);
